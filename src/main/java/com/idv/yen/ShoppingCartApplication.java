b253b28926f77c386cbf47750406bd2db42d7e4c
YHYen <cloudyhyen@gmail.com>
2023-02-24 15:50:42 +0800

    1. Set up HTTPS connection 2. Prepare to display videos and images

--- src/main/java/com/idv/yen/ShoppingCartApplication.java
+++ src/main/java/com/idv/yen/ShoppingCartApplication.java
@@ -1,12 +1,37 @@
 package com.idv.yen;
 
+import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication (scanBasePackages = "com.idv.yen")
 @MapperScan("com.idv.yen.mapper")
 public class ShoppingCartApplication {
+
+    /**
+     * Configure the built-in servlet container factory as tomcat.
+     *
+     * @return ServletWebServerFactory
+     * */
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcatServletWebServerFactory;
+    }
+
+    public Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
